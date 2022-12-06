31e814ee24e1fc3845931303fb59329c8726c382
ianabreu <ianabreudev@gmail.com>
2022-12-06 04:26:23 -0300

    adicionando states aos inputs de cadastro

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,47 +1,50 @@
 import React, {useState} from 'react';
-import { ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { ScrollView, View } from 'react-native';
 import { Container, Title, Description, Input, CenterView, Button, ButtonText } from './styles';
 
 export default function New() {
-    const [book, setBook] = useState({
-        title: '',
-        serie: '',
-        author: '',
-        vol: '',
-        price: '',
-        description: '',
-    })
+    const [title, setTitle] = useState('');
+    const [serie, setSerie] = useState('');
+    const [author, setAuthor] = useState('');
+    const [vol, setVol] = useState('');
+    const [price, setPrice] = useState('');
+    const [description, setDescription] = useState('');
+
+    function addBook() {
+        alert('clicou')
+    }
+
     return (
         <ScrollView>
 
             <Container>
                 <Title>Titulo</Title>
-                <Input placeholder='Título do livro' value={book.title} onChangeText={(text) => {setBook(text)}}/>
+                <Input placeholder='Título do livro' value={title} onChangeText={text => setTitle(text)}/>
 
                 <Title>Série</Title>
-                <Input placeholder='Série de livros' value={book.serie} onChangeText={(text) => {setBook(text)}}/>
+                <Input placeholder='Série de livros' value={serie} onChangeText={text => setSerie(text)}/>
 
                 <Title>Autor</Title>
-                <Input  placeholder='Autor' value={book.author} onChangeText={(text) => {setBook(text)}}/>
+                <Input  placeholder='Autor' value={author} onChangeText={text => setAuthor(text)}/>
 
                 <View style={{ flexDirection: 'row',  width: '100%', justifyContent:'space-between' }}>
 
                     <View style={{ flexDirection: 'column', width:'45%' }}>
                         <Title>Volume</Title>
-                        <Input placeholder='1' value={book.vol} onChangeText={(text) => {setBook(text)}}/>
+                        <Input placeholder='1' value={vol} onChangeText={text => setVol(text)}/>
                     </View>
 
                     <View style={{ flexDirection: 'column', width:'45%' }}>
                         <Title>Preço</Title>
-                        <Input placeholder='R$ 0.00' value={book.price} onChangeText={(text) => {setBook(text)}}/>
+                        <Input placeholder='R$ 0.00' value={price} onChangeText={text => setPrice(text)}/>
                     </View>
                 </View>
 
                 <Title>Descrição</Title>
-                <Description placeholder='Descrição do livro...' value={book.description} onChangeText={(text) => {setBook(text)}}/>
+                <Description placeholder='Descrição do livro...' value={description} onChangeText={text => setDescription(text)}/>
 
                 <CenterView>
-                    <Button>
+                    <Button onPress={addBook}>
                         <ButtonText>Cadastrar</ButtonText>
                     </Button>
 
